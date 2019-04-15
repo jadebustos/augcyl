6040a4a35a17bb4833b9616e0bf2da382c0e3f0f
Jose Angel de Bustos Perez <jadebustos@gmail.com>
2019-04-15 16:09:39 +0200

    salamanca 2007 old delete

--- 2007-salamanca-curso-usal/administracion-avanzada-del-sistema/scripts/txtunixdir.sh
+++
@@ -1,20 +0,0 @@
-#!/bin/bash
-for fichero in *.txt; do
-   if grep  $fichero &>/dev/null; then
-     resp=x
-     while [ $resp != "s" -a $resp != "n" ]; do
-       echo "'$fichero' es un fichero texto DOS. convertir? (s/n) "
-       read resp
-     done
-     case $resp in
-       s)
-         sed 's///' $fichero > /tmp/FILE_TMP
-         mv /tmp/FILE_TMP $fichero
-	 echo "El fichero '$fichero' convertido a texto UNIX";;
-       n)
-         echo "El fichero '$fichero' se deja texto DOS";;
-       *)
-         echo "ERROR";;
-     esac
-   fi
-done
